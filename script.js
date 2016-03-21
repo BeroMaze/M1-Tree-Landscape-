5679c1fe8166c7930110d6743363a14dbd2c51dd
BeroMaze <Berning.corey@gmail.com>
2016-03-21 11:08:01 -0700

    create express and mail server

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $('.aboutBoxes').hide();
 $('#ContactForm').hide();
+$('#ordinancesTable').hide();
 
 $('li').hover(function() {
   $(this).toggleClass('pulse animated');
@@ -11,11 +12,52 @@ $('#contact').on('click', function(event) {
   $('#about').hide();
   $('#about').hide();
   $('#about').hide();
+  $('#ordinancesTable').hide();
+  history.pushState("contactus ","contactus","contactus");
+});
+
+$('#ordinance').on('click', function(event) {
+  $('#ordinancesTable').show();
+  $('#ContactForm').hide();
+  $('#logoLanding').hide();
+  $('#about').hide();
+  $('#about').hide();
+  $('#about').hide();
+  history.pushState("ordinances","ordinances","ordinances");
 });
 
 $('#home').on('click', function(event) {
-  window.open("index.html","_self")
+  window.open("index.html","_self");
 });
+
+$('#submit').on('click', function(event) {
+  event.preventDefault();
+  var form = $('#contactus').serializeArray();
+  var object = {
+    firstName: form[0].value,
+    lastName: form[1].value,
+    email: form[2].value,
+    number: form[3].value,
+    message: form[4].value
+  };
+  console.log(object);
+  $.post('/contact', {info: object}, function(data) {
+  }).done(function(data) {
+    if (data.flag === true) {
+      $('#form').empty();
+      $('#form').append("<h1 class='formData'>"+ data.message +"</h1>");
+      $('#form').append('<img class="dataImg" src="/images/dataImg.jpg">');
+      $('#form').addClass('dataToCenter');
+    }
+    else {
+      alert('working');
+      $('#formTitle').html(data.message);
+
+    }
+
+  });
+});
+
 function sizing() {
   $width = $(window).width();
   $height = $(window).height();
@@ -33,8 +75,22 @@ function sizing() {
   $('#logo').css('height', $height);
   $('#ContactForm').css('width', $width);
   $('#ContactForm').css('height', $height-90);
+  $('#ordinancesTable').css('width', $width);
+  $('#ordinancesTable').css('height', $height);
+  if ($width > 640) {
+    $('li').css('display', 'inline-block');
+    $('nav').css('width', '50%');
+  }
+  else {
+    $('li').css('display', 'none');
+    $('nav').css('width', '100px');
+  }
 }
 sizing();
+
+$(window).resize(function(event) {
+  sizing();
+});
 
 $('main').scroll(function() {
    if($('main').scrollTop() == $(window).height()) {
@@ -44,3 +100,51 @@ $('main').scroll(function() {
      $('.aboutBoxes').show();
    }
 });
+
+$('nav').hover(function() {
+  if ($(window).width() <= 640) {
+    $('li').css('display', 'inline-block');
+    $('#title').css('display', 'none');
+    $('nav').css({'width': '100%',
+                  'background-color': 'rgba(0, 0, 0, 0.54)',
+                  'color': 'white',
+    });
+    $('#navBurg').css({'filter': 'invert(100%)',
+                      '-webkit-filter': 'invert(100%)',
+                      '-moz-filter': 'invert(100%)',
+                      'width': '78px'
+    });
+  }
+}, function() {
+  if ($(window).width() <= 640) {
+    $('li').css('display', 'none');
+    $('#title').css('display', 'block');
+    $('nav').css({'width': '100px',
+                  'background-color': 'rgba(0, 0, 0, 0)',
+                  'color': 'black',
+    });
+    $('#navBurg').css({'filter': 'invert(0%)',
+                      '-webkit-filter': 'invert(0%)',
+                      '-moz-filter': 'invert(0%)',
+                      'width': '300px'
+    });
+  }
+});
+
+if(window.location.href.indexOf('contactus') > -1){
+  $('#ContactForm').show();
+  $('#logoLanding').hide();
+  $('#about').hide();
+  $('#about').hide();
+  $('#about').hide();
+  $('#ordinancesTable').hide();
+}
+
+if(window.location.href.indexOf('ordinances') > -1){
+  $('#ordinancesTable').show();
+  $('#ContactForm').hide();
+  $('#logoLanding').hide();
+  $('#about').hide();
+  $('#about').hide();
+  $('#about').hide();
+}
---
+++ server.js
@@ -0,0 +1,70 @@
+"use strict"
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+var PORT = 3000;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static('./'));
+app.use(bodyParser());
+
+app.get('*', function(request, response) {
+  console.log('New request:', request.url);
+  response.sendFile('index.html', { root: '.' });
+});
+
+app.get('/contactus/', function(req, res) {
+  res.sendFile('index.html', {root:__dirname + '/'});
+});
+
+app.post('/contact', function (req, res) {
+  console.log(req.body.info);
+  var form = req.body.info;
+  var mailOpts, smtpTrans;
+  //Setup Nodemailer transport, I chose gmail. Create an application-specific password to avoid problems.
+  smtpTrans = nodemailer.createTransport('SMTP', {
+      service: 'Gmail',
+      auth: {
+          user: "m1servermail@gmail.com",
+          pass: "treeandlandscape"
+      }
+  });
+  //Mail options
+  mailOpts = {
+      from: form.firstName + ' ' +  form.lastName + '; '+ form.email + '', //grab form data from the request body object
+      to: 'berning.corey@gmail.com',
+      subject: 'M1 Contact Form',
+      html:form.message + '<br><br>' + form.firstName + ' ' +  form.lastName + '<br>' + form.number  + '<br>' + form.email
+  };
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+      //Email not sent
+      if (error) {
+        console.log(error);
+        var data = {
+          message: 'Sorry there was an issue.<br> Please try submitting your message again. Thank you.',
+          flag: false
+        };
+        res.send(data);
+      }
+      //Yay!! Email sent
+      else {
+        var data = {
+          message: 'Your message has been sent.<br> Thank you for contacting M1, we will be in contact soon.',
+          flag: true
+        };
+        console.log('sent');
+        res.send(data);
+      }
+  });
+});
+
+  app.listen(PORT, function() {
+    console.log('server started');
+    console.log('listening on PORT: ', + PORT);
+  });
